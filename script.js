552dee7bcb31785756e73382d4910d06ec84e550
anet-eny <anet.eny@gmail.com>
2024-09-16 20:15:02 +0200

    Add basic consts and verify if the colorInput works

---
+++ script.js
@@ -0,0 +1,10 @@
+const colorInput = document.getElementById('color-input')
+const modes = document.getElementById('modes-select')
+const btn = document.getElementById('get-scheme-btn')
+
+
+
+
+btn.addEventListener("click", (event) => {
+    console.log(colorInput.value)
+})
