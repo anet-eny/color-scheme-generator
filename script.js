cf24098fc020800edb1d7eb174064c6fabde5922
anet-eny <anet.eny@gmail.com>
2024-09-16 21:40:25 +0200

    Update renderColors() and display resulting colors on page

--- script.js
+++ script.js
@@ -7,11 +7,23 @@ function getColors(seed, mode) {
     fetch(`https://www.thecolorapi.com/scheme?hex=${seed.slice(1)}&mode=${mode}`)
     .then (res => res.json())
     .then (data => {
-        console.log(data.colors)
+        renderColors(data.colors)
     })
 }
 
-btn.addEventListener("click", (event) => {
+function renderColors(colorsArr) {
+    let html = ""
+    for (let color of colorsArr) {
+        html += `
+        <div class="color" style=background-color:${color.hex.value}></div>
+        `
+    }
+    console.log(html)
+    document.getElementById('colors-container').innerHTML = html
+    
+}
+
+btn.addEventListener("click", () => {
     let seedColor = colorInput.value
     let selectedMode = modes.value
     getColors(seedColor, selectedMode)
