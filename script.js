bf5e61f3a338098efe765e37c2bdd9a43a5ac239
anet-eny <anet.eny@gmail.com>
2024-09-16 20:40:27 +0200

    Create getColors() and separate fetch() from event listener

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const modes = document.getElementById('modes-select')
 const btn = document.getElementById('get-scheme-btn')
 
 
-
+function getColors(seed) {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${seed.slice(1)}`)
+    .then (res => res.json())
+    .then (data => {
+        console.log(data)
+    })
+}
 
 btn.addEventListener("click", (event) => {
-    console.log(colorInput.value)
+    let seedColor = colorInput.value
+    getColors(seedColor)
+   
 })
+
